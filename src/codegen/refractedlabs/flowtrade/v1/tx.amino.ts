f506b9bd4cf2ef668de37980d258de6877a49789
Amin Abbasi <amin@refractedlabs.com>
2023-12-28 14:44:51 +0330

    chore: update flowtrade proto files and regenerate
    CU-86bww48r3

--- src/codegen/refractedlabs/flowtrade/v1/tx.amino.ts
+++ src/codegen/refractedlabs/flowtrade/v1/tx.amino.ts
@@ -2,42 +2,42 @@
 import { MsgUpdateParams, MsgCreateFlow, MsgJoinFlow, MsgExitFlow, MsgSetOperator, MsgClaimTokenIn, MsgClaimTokenOut, MsgStopFlow } from "./tx";
 export const AminoConverter = {
   "/refractedlabs.flowtrade.v1.MsgUpdateParams": {
-    aminoType: "refractedlabs/flowtrade/v1/UpdateParams",
+    aminoType: "refracted/flowtrade/v1/UpdateParams",
     toAmino: MsgUpdateParams.toAmino,
     fromAmino: MsgUpdateParams.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgCreateFlow": {
-    aminoType: "refractedlabs/flowtrade/v1/CreateFlow",
+    aminoType: "refracted/flowtrade/v1/CreateFlow",
     toAmino: MsgCreateFlow.toAmino,
     fromAmino: MsgCreateFlow.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgJoinFlow": {
-    aminoType: "refractedlabs/flowtrade/v1/JoinFlow",
+    aminoType: "refracted/flowtrade/v1/JoinFlow",
     toAmino: MsgJoinFlow.toAmino,
     fromAmino: MsgJoinFlow.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgExitFlow": {
-    aminoType: "refractedlabs/flowtrade/v1/ExitFlow",
+    aminoType: "refracted/flowtrade/v1/ExitFlow",
     toAmino: MsgExitFlow.toAmino,
     fromAmino: MsgExitFlow.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgSetOperator": {
-    aminoType: "refractedlabs/flowtrade/v1/SetOperator",
+    aminoType: "refracted/flowtrade/v1/SetOperator",
     toAmino: MsgSetOperator.toAmino,
     fromAmino: MsgSetOperator.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgClaimTokenIn": {
-    aminoType: "refractedlabs/flowtrade/v1/ClaimTokenIn",
+    aminoType: "refracted/flowtrade/v1/ClaimTokenIn",
     toAmino: MsgClaimTokenIn.toAmino,
     fromAmino: MsgClaimTokenIn.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgClaimTokenOut": {
-    aminoType: "refractedlabs/flowtrade/v1/ClaimTokenOut",
+    aminoType: "refracted/flowtrade/v1/ClaimTokenOut",
     toAmino: MsgClaimTokenOut.toAmino,
     fromAmino: MsgClaimTokenOut.fromAmino
   },
   "/refractedlabs.flowtrade.v1.MsgStopFlow": {
-    aminoType: "refractedlabs/flowtrade/v1/StopFlow",
+    aminoType: "refracted/flowtrade/v1/StopFlow",
     toAmino: MsgStopFlow.toAmino,
     fromAmino: MsgStopFlow.fromAmino
   }
